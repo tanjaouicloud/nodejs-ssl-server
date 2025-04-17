540ccc6076c17abb184230df4f7eda98ece76503
Deepshiga Raveendran <deepshigaravee@gmail.com>
2025-04-17 19:13:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1'; // Your server ip address
+const hostname = '0.0.0.0'; // Your server ip address
 const port = 3000;
 
 const version = '1.0.0';
